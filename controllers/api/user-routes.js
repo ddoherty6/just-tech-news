24cac2290dc2a8f85e3125cf1591f68ea2621f14
Daniel <ddoherty6@gmail.com>
2022-02-07 13:47:29 -0800

    added sessions and login/logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const { User, Post, Vote } = require('../../models');
 
-// get all users
+// get all users (/api/users/)
 router.get('/', (req, res) => {
   User.findAll({
     attributes: { exclude: ['password'] }
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      console.log("IN USER GET /");
+      res.json(dbUserData);})
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -54,6 +56,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// (/api/users/)
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -61,7 +64,15 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -87,7 +98,14 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
 });
 
@@ -131,6 +149,17 @@ router.delete('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
