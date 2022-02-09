db28540ba0cad38a56d4674a9ec3fd6cbb81eac5
Daniel <ddoherty6@gmail.com>
2022-02-08 16:31:05 -0800

    added single post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -92,13 +92,17 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+   // make sure the session exists first
+   console.log(req.body);
+   if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ post_id: req.body.post_id, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put('/:id', (req, res) => {
